078db672738822e7f82f83d9a8ca31016cea7dba
Dakdan <khowit7@gmail.com>
2026-01-15 12:08:17 +0700

    Update config.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// ===== GLOBAL CONFIG =====
-const API =
-"https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
