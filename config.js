e422517625c37f0ee31bd19bbf6d81d59aa87aa7
Dakdan <khowit7@gmail.com>
2026-01-15 12:36:58 +0700

    Update config.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- config.js
+++ config.js
@@ -1,18 +1,12 @@
-function api(action, data) {
-  return new Promise((resolve) => {
+// config.js
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
 
-    // mock user
-    if (action === "login") {
-      if (data.username === "admin" && data.password === "1234") {
-        resolve({
-          success: true,
-          role: "ADMIN",
-          dept: "IT"
-        });
-      } else {
-        resolve({ success: false });
-      }
+function api(action, data = {}) {
+  return fetch(GAS_URL, {
+    method: "POST",
+    body: JSON.stringify({ action, ...data }),
+    headers: {
+      "Content-Type": "application/json"
     }
-
-  });
+  }).then(r => r.json());
 }
