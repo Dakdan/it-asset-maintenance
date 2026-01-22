85032e32381c7e5a2ad8a7f74c53be824aebbc46
Dakdan <khowit7@gmail.com>
2026-01-22 08:43:41 +0700

    Update api.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- js/api.js
+++ js/api.js
@@ -1,7 +1,12 @@
 const API =
 "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
 
-function apiGet(a){return fetch(API+"?action="+a).then(r=>r.json())}
+function apiGet(a){
+  return fetch(API+"?action="+a).then(r=>r.json())
+}
 function apiPost(d){
- return fetch(API,{method:"POST",body:JSON.stringify(d)}).then(r=>r.json())
+  return fetch(API,{
+    method:"POST",
+    body:JSON.stringify(d)
+  }).then(r=>r.json())
 }
